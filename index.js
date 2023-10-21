f75a5369b4370611dff23df71f92044e3fba02df
TanimShahriar <ssmtsshr@gmail.com>
2023-10-21 14:11:05 +0600

    backend data written for add to cart section

--- index.js
+++ index.js
@@ -122,6 +122,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete("/addtocart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
